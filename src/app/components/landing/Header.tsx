824a960839e57ca972534f8fecfa6dc2f3e2e0c6
jonny.langefeld <jonny.langefeld@gmail.com>
2023-08-08 08:41:53 -0700

    Add logo

--- src/app/components/landing/Header.tsx
+++ src/app/components/landing/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Button from "@/app/components/Button"
+import Logo from "../Logo"
 
 export default function Header() {
   return (
@@ -9,7 +10,9 @@ export default function Header() {
       }
     >
       <nav className="flex items-center justify-between space-x-4 p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">nextUX</div>
+        <div className="flex lg:flex-1">
+          <Logo className="h-10 w-64" />
+        </div>
 
         <div className="xs:space-x-2 flex flex-1 items-center justify-end space-x-4">
           <Button gradient>Sign Up</Button>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,26 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "nextUX",
   description: "Make human input more natural",
+  icons: {
+    icon: { url: "/favicon.svg", type: "image/svg" },
+    shortcut: { url: "/favicon.svg", type: "image/svg" },
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`${inter.className} text-gray-900 last:overflow-hidden dark:text-gray-200`}>{children}</body>
     </html>
   )
