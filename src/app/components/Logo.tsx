f4e8a994f48d955d7ffc2a9edf69e8dd427e6e0d
Jonny <jonny.langefeld@gmail.com>
2023-08-08 08:43:27 -0700

    Add logo (#6)

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import React from "react"
+
+interface Props {
+  className?: string
+}
+
+export default function Logo({ className }: Props) {
+  return (
+    <div className={`relative ${className}`}>
+      <object
+        type="image/svg+xml"
+        title="nextux Logo"
+        data="/logo.svg"
+        className="absolute inset-0 h-full w-full"
+        role="img"
+        style={{
+          backgroundColor: "transparent",
+          colorScheme: "light",
+        }}
+      ></object>
+      <Link href="/" className="absolute inset-0" />
+    </div>
+  )
+}
--- src/app/components/landing/Section.tsx
+++ src/app/components/landing/Section.tsx
@@ -30,10 +30,7 @@ export default function Section(props: Props) {
   return (
     <div className="h-screen bg-white dark:bg-slate-900">
       <div className="relative isolate flex h-full items-center justify-center px-6 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
+        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu blur-3xl sm:-top-80" aria-hidden="true">
           <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-50 transition-all duration-[10000ms] ease-background-polygon dark:opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
@@ -43,11 +40,11 @@ export default function Section(props: Props) {
         </div>
         {props.children}
         <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-50 transition-all duration-[10000ms] ease-background-polygon dark:opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-50 transition-all duration-[10000ms] ease-background-polygon dark:opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath: randomPolygon,
             }}
