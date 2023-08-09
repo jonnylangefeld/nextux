cbd5c5bd4ae1293d8935bbee50012dedd1f021e3
Jonny <jonny.langefeld@gmail.com>
2023-08-08 17:33:59 -0700

    Add preview images (#7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "nextUX",
   description: "Make human input more natural",
+  content: "Make human input more natural",
   icons: {
     icon: { url: "/favicon.svg", type: "image/svg" },
     shortcut: { url: "/favicon.svg", type: "image/svg" },
@@ -25,6 +26,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
+        <meta property="og:title" content="nextUX" />
+        <meta property="og:description" content="Make human input more natural" />
+        <meta property="og:url" content="https://nextux.ai" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="nextUX" />
+        <meta property="og:type" content="website" />
       </head>
       <body className={`${inter.className} text-gray-900 last:overflow-hidden dark:text-gray-200`}>{children}</body>
     </html>
