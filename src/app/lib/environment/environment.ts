7c951b48739a33b091dab7741a13b4045b2e87f4
jonny.langefeld <jonny.langefeld@gmail.com>
2023-09-23 19:51:09 -0700

    Refactor MagicForm.tsx
    clean up the file a little

--- src/app/lib/environment/environment.ts
+++ src/app/lib/environment/environment.ts
@@ -13,9 +13,5 @@ export function requireEnvVariable(name: string): string {
 }
 
 export const env = {
-  GOOGLE_APPLICATION_CREDENTIALS_JSON: requireEnvVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON"),
-  // GCS_BUCKET: requireEnvVariable("GCS_BUCKET"),
   OPENAI_API_KEY: requireEnvVariable("OPENAI_API_KEY"),
-  // NEXT_PUBLIC_SUPABASE_URL: requireEnvVariable("NEXT_PUBLIC_SUPABASE_URL"),
-  // SUPABASE_SERVICE_ROLE_KEY: requireEnvVariable("SUPABASE_SERVICE_ROLE_KEY"),
 }
