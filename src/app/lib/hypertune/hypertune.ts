3aecbad6527733accbd24317ed81936d0a46f73e
jonny.langefeld <jonny.langefeld@gmail.com>
2023-09-23 23:03:18 -0700

    Add feature flags to server

--- src/app/lib/hypertune/hypertune.ts
+++ src/app/lib/hypertune/hypertune.ts
@@ -1,5 +1,19 @@
-import { initializeHypertune } from "@/app/lib/hypertune/flags"
+import { initializeHypertune, Rec2 } from "@/app/lib/hypertune/flags"
 
 const hypertune = initializeHypertune({}, { token: process.env.NEXT_PUBLIC_HYPERTUNE_TOKEN })
 
 export default hypertune
+
+export const context: Rec2 = {
+  context: {
+    environment: process.env.NODE_ENV,
+  },
+}
+
+/**
+ * The Hypertune flags. Use this only on the server.
+ */
+export const flags = async () => {
+  await hypertune.initFromServerIfNeeded()
+  return hypertune.root(context)
+}
