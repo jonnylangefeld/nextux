5b2b020e4f851f7b5336d46d82c4566f6fe5e52c
jonny.langefeld <jonny.langefeld@gmail.com>
2023-10-06 07:07:26 -0700

    Finally this is a working example in safari with mp3

--- src/app/components/RecordButton.tsx
+++ src/app/components/RecordButton.tsx
@@ -1,53 +1,64 @@
 "use client"
 
+import AudioRecorder from "audio-recorder-polyfill"
+import mpegEncoder from "audio-recorder-polyfill/mpeg-encoder"
 import React from "react"
-import RecordRTC from "recordrtc"
 import Button from "../components/Button"
 import { ExtractRequest } from "../lib/proto/types"
 import { contactFormSchema } from "../lib/rjsfSchemas"
 
+AudioRecorder.encoder = mpegEncoder
+AudioRecorder.prototype.mimeType = "audio/mpeg"
+window.MediaRecorder = AudioRecorder
+
 export default function RecordButton() {
+  const [recording, setRecording] = React.useState(false)
+  const [recorder, setRecorder] = React.useState<MediaRecorder | null>(null)
+
   const onClick = async () => {
+    if (recording) {
+      setRecording(false)
+      recorder?.stop()
+      recorder?.stream.getTracks().forEach((i) => i.stop())
+    }
     navigator.mediaDevices
       .getUserMedia({ audio: true })
       .then(async (stream) => {
-        const recorder = new RecordRTC.StereoAudioRecorder(stream.clone(), {
-          type: "audio",
-          mimeType: "audio/webm;codecs=pcm",
-          numberOfAudioChannels: 1,
-        })
-        recorder.record()
+        const recorder = new MediaRecorder(stream)
+        setRecorder(recorder)
 
-        // sleep 5 seconds
-        await new Promise((r) => setTimeout(r, 5000))
+        recorder.addEventListener("dataavailable", (event) => {
+          console.log(event.data)
 
-        recorder.stop(function (data) {
-          console.log(data)
-          const reader = new FileReader()
+          if (event.data.size > 0) {
+            const reader = new FileReader()
 
-          reader.onloadend = async function () {
-            if (reader.result) {
-              const base64Audio = (reader.result as string).split(",")[1] // Splitting to get only the Base64 data
+            reader.onloadend = async function () {
+              if (reader.result) {
+                const base64Audio = (reader.result as string).split(",")[1] // Splitting to get only the Base64 data
 
-              const body: ExtractRequest = {
-                jsonSchema: Buffer.from(JSON.stringify(contactFormSchema)).toString("base64"),
-                document: base64Audio,
+                const body: ExtractRequest = {
+                  jsonSchema: Buffer.from(JSON.stringify(contactFormSchema)).toString("base64"),
+                  document: base64Audio,
+                }
+
+                console.log(body)
+                const resp = await fetch("/api/extract", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(body),
+                })
               }
-
-              console.log(body)
-              const resp = await fetch("/api/extract", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(body),
-              })
             }
-          }
 
-          reader.readAsDataURL(data)
-          // You can now use the blob, e.g., save it, upload it, etc.
+            reader.readAsDataURL(event.data)
+          }
         })
+
+        recorder.start()
+        setRecording(true)
       })
       .catch(function (error) {
         console.error("Media device access denied:", error)
